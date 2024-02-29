2d2d315dd40f8745b8588cd5dc26b8c9e9efccd8
sakshi028 <96768012+sakshi028@users.noreply.github.com>
2024-02-29 08:08:18 +0000

    1st javascript file

---
+++ Object.js
@@ -0,0 +1,18 @@
+console.log("sakshi dighade");
+
+//destructuring of objects
+
+const ClassRoom={
+    table:"plywood",
+    fan:"bajyaj",
+    pc:"Dell",
+    Book:"classmates"
+}
+const {Book:items}=ClassRoom
+console.log(items)
+
+//json array
+[
+    {},
+    {}
+]
